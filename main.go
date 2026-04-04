13b4f6e7128b87256346d96ead9fc8d74b3eaadf
Arafat Hasan <nayemarafat786@gmail.com>
2026-04-04 15:19:51 +0600

    Added JSON tags to validate

--- main.go
+++ main.go
@@ -23,6 +23,8 @@ func main() {
 	mux.Handle("/app/", fsHandler)
 
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
+
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
